27835d9e3f975b646a6ca5f9b18a4553a227cfc3
cuijh <3298060009@qq.com>
2024-10-12 11:27:32 +0000

    init

--- inf_sr.sh
+++ inf_sr.sh
@@ -1,6 +1,6 @@
 a="$1"
 b="$2"
-cd /cpfs01/projects-HDD/cfff-6f3a36a0cd1e_HDD/public/Hallo/workspace/cuijh/open-source/hallo
+cd /cpfs01/projects-HDD/cfff-6f3a36a0cd1e_HDD/public/Hallo/workspace/cuijh/open-source/hallo2
 
 # source .venv/bin/activate
-python scripts/video_sr.py --bg_upsampler realesrgan --face_upsample -w 1 --input_path "$a" -s 4  
+/cpfs01/projects-HDD/cfff-6f3a36a0cd1e_HDD/public/Hallo/workspace/cuijh/env/hallo/bin/python scripts/video_sr.py --bg_upsampler realesrgan --face_upsample -w 1 --input_path "$a" -s 4 --output_path "$b" 
